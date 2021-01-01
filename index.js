7c054295a132850c32a0bb4aae0875db69b5cf27
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-01-01 14:14:50 +0530

    Configure CORS headers

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const userRoutes = require("./Routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(cookieParser());
 
